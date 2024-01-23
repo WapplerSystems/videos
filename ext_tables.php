ba419c5a4cbba9e797834f363f6768e43de20df2
Sven Wappler <info@wappler.systems>
2024-01-23 13:19:16 +0100

    add backend css to set videos to 100% width in preview

--- ext_tables.php
+++ ext_tables.php
@@ -7,6 +7,11 @@ call_user_func(
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', '[videos] video player');
 
+
+
+        $GLOBALS['TBE_STYLES']['skins']['backend']['stylesheetDirectories']['videos'] = 'EXT:videos/Resources/Public/CSS/Backend/';
+
+
     },
     'videos'
 );
