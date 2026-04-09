7cd6fbb2c046998090a6879bf3630dafd65694a9
Sven Wappler <wappler@wappler.systems>
2026-04-09 12:51:39 +0200

    Fix: Replace renderStatic() with render() for Fluid v4 / TYPO3 v14 compatibility

--- Classes/ViewHelpers/AspectRatioClassViewHelper.php
+++ Classes/ViewHelpers/AspectRatioClassViewHelper.php
@@ -11,41 +11,23 @@ declare(strict_types=1);
 namespace WapplerSystems\Videos\ViewHelpers;
 
 use TYPO3\CMS\Core\Resource\FileInterface;
-use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
-/**
- * Class AspectRatioClassViewHelper
- *
- * @phpstan-type ArgumentsArray array{
- *     aspectRatio: FileInterface|string|null,
- *     default: string|null
- * }
- */
 class AspectRatioClassViewHelper extends AbstractViewHelper
 {
-    #[\Override]
     public function initializeArguments(): void
     {
         parent::initializeArguments();
 
-        $this->registerArgument('aspectRatio', 'mixed', 'The desired aspect ratio, can either be a FileInterface instance or an string. If omitted, $renderChildrenClosure() will be executed to be used for inline notation.', false);
+        $this->registerArgument('aspectRatio', 'mixed', 'The desired aspect ratio, can either be a FileInterface instance or a string.', false);
         $this->registerArgument('default', 'string', 'If no valid aspectRatio is given, this value will be used.', false);
     }
 
-    /**
-     * @param ArgumentsArray $arguments
-     */
-    public static function renderStatic(
-        array $arguments,
-        \Closure $renderChildrenClosure,
-        RenderingContextInterface $renderingContext
-    ): string {
-        $defaultValue = (string)($arguments['default'] ?? '16:9');
-        $aspectRatio = $arguments['aspectRatio'] ?? $renderChildrenClosure();
+    public function render(): string
+    {
+        $defaultValue = (string)($this->arguments['default'] ?? '16:9');
+        $aspectRatio = $this->arguments['aspectRatio'] ?? $this->renderChildren();
 
-        // First check if the given aspectRatio argument is an file interface.
-        // If so, read the "aspect_ratio" property from it
         if (
             $aspectRatio instanceof FileInterface
             && $aspectRatio->hasProperty('aspect_ratio')
@@ -53,15 +35,12 @@ class AspectRatioClassViewHelper extends AbstractViewHelper
             $aspectRatio = $aspectRatio->getProperty('aspect_ratio');
         }
 
-        // If the given aspectRatio argument is not a string or not set, use the default value
-        if (! is_string($aspectRatio) || empty($aspectRatio)) {
+        if (!is_string($aspectRatio) || empty($aspectRatio)) {
             $aspectRatio = $defaultValue;
         }
 
-        // Now it's time to replace the colon with a minus, so it'll be compatible with VideoJS
         $aspectRatio = str_replace(':', '-', $aspectRatio);
 
-        // Last but not least, add the VideoJs prefix
         return 'vjs-' . $aspectRatio;
     }
 }
